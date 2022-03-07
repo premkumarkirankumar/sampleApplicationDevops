df569591218f31526bc17a41d4ae910291b06659
Kiran Kumar Prem Kumar <premkumarkirankumar@gmail.com>
2022-03-06 23:54:00 -0500

    push changes

--- src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
@@ -86,7 +86,7 @@ public class OwnerController {
             owner.setLastName(""); // empty string signifies broadest possible search
         }
 
-        // find owners by last name
+        // Find 1Owners by last name
         Collection<Owner> results = this.clinicService.findOwnerByLastName(owner.getLastName());
         if (results.isEmpty()) {
             // no owners found
