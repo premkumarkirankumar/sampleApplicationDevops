730bd73aa624ecefc5c76688ab0aff686b8f6e15
kiran <kiran5688@gmail.com>
2022-03-10 08:03:41 -0500

    updated code

--- src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/web/OwnerController.java
@@ -86,7 +86,7 @@ public class OwnerController {
             owner.setLastName(""); // empty string signifies broadest possible search
         }
 
-        // FIND 6OWNERS by last name
+        // FIND 7OWNERS by last name
         Collection<Owner> results = this.clinicService.findOwnerByLastName(owner.getLastName());
         if (results.isEmpty()) {
             // no owners found
